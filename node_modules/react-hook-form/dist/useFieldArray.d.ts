69c230a48c78eba927725286babdc88eddcb927e
Irfan Asif <irfan_2020bece060@nitsri.net>
2022-10-23 23:21:48 +0530

    build: npm → yarn

--- node_modules/react-hook-form/dist/useFieldArray.d.ts
+++ node_modules/react-hook-form/dist/useFieldArray.d.ts
@@ -1,6 +1,6 @@
 import { FieldArrayPath, FieldValues, UseFieldArrayProps, UseFieldArrayReturn } from './types';
 /**
- * A custom hook that exposes convenient methods to perform operations with a list of dynamic inputs that need to be appended, updated, removed etc.
+ * A custom hook that exposes convenient methods to perform operations with a list of dynamic inputs that need to be appended, updated, removed etc. • [Demo](https://codesandbox.io/s/react-hook-form-usefieldarray-ssugn) • [Video](https://youtu.be/4MrbfGSFY2A)
  *
  * @remarks
  * [API](https://react-hook-form.com/api/usefieldarray) • [Demo](https://codesandbox.io/s/react-hook-form-usefieldarray-ssugn)
